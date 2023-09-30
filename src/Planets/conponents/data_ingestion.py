a07251dd204aeb5a99bd96fd194e606eaae8061b
Doğuhan İlter <doguhannilt@gmail.com>
2023-09-30 16:39:03 +0300

    Data Transformation & Codecounter.py & Additional.py

--- src/Planets/conponents/data_ingestion.py
+++ src/Planets/conponents/data_ingestion.py
@@ -2,13 +2,31 @@ import pandas as pd
 from dataclasses import dataclass
 import os
 from sklearn.model_selection import train_test_split # Dividing into X_train, X_test, y_train,y_test
-
+import zipfile
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path: str=os.path.join('artifacts',"train.csv")
-    test_data_path: str=os.path.join('artifacts',"test.csv")
-    raw_data_path: str=os.path.join('artifacts',"data.csv")
+    base_data_path: str = '../../../artifacts/'
+    X_train_data_path: str = os.path.join(base_data_path, 'X_train.csv')
+    X_test_data_path: str = os.path.join(base_data_path, 'X_test.csv')
+    y_test_data_path: str = os.path.join(base_data_path, 'y_test.csv')
+    y_train_data_path: str = os.path.join(base_data_path, 'y_train.csv')
+
+
+
+class Data:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def Dataextraction(self):
+        # Extract the zip file
+        with zipfile.ZipFile('C:/Users/doguy/Desktop/Planets/data.zip', 'r') as zip_ref:
+            zip_ref.extractall('C:/Users/doguy/Desktop/Planets/artifacts/')
+
+        data = pd.read_csv("C:/Users/doguy/Desktop/Planets/artifacts/data.csv")
+        os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+        data.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
 
 class DataIngestion:
     def __init__(self):
@@ -16,24 +34,26 @@ class DataIngestion:
 
     def initiate_data_ingestion(self):
             
-            df=pd.read_csv('artifacts/designed_data.csv')
+        os.makedirs(self.ingestion_config.base_data_path, exist_ok=True)
+        
+        df = pd.read_csv(os.path.join(self.ingestion_config.base_data_path, 'designed_data.csv'))
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+        X = df.drop("Planet Name", axis = 1)
+        y = df["Planet Name"]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
-            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            #-> Designed Data Transformation train_set.drop("Unnamed: 0",axis = 1,inplace=True)
+            #-> Designed Data Transformation test_set.drop("Unnamed: 0",axis = 1,inplace=True)
+        X_train.to_csv(self.ingestion_config.X_train_data_path,index=False,header=True)
+        X_test.to_csv(self.ingestion_config.X_test_data_path,index=False,header=True)
 
+        y_train.to_csv(self.ingestion_config.y_train_data_path,index=False,header=True)
+        y_test.to_csv(self.ingestion_config.y_test_data_path,index=False,header=True)
 
-            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+        return(
+            self.ingestion_config.X_train_data_path,
+            self.ingestion_config.y_test_data_path,
+            self.ingestion_config.y_train_data_path,
+            self.ingestion_config.X_test_data_path
+        )
 
-            train_set.drop("Unnamed: 0",axis = 1,inplace=True)
-            test_set.drop("Unnamed: 0",axis = 1,inplace=True)
-
-            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
-
-            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
-
-            return(
-                self.ingestion_config.train_data_path,
-                self.ingestion_config.test_data_path
-            )
--- src/Planets/conponents/data_transformation.py
+++ src/Planets/conponents/data_transformation.py
@@ -1,7 +1,36 @@
-# In trials.ipynb, practice feature selection
-
 # Clean the train.csv --> Dropping (The columns that has more than 1000 null value and unecessary columns)
 # Clean the train.csv --> Dropping (The columns that has more than 1000 null value and unecessary columns)
 # Median Imputation (to numerical columns)
 # Categorical Frequency Imputation (to categorical columns)
 # y = 'Planet Name'
+from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+import os
+from dataclasses import dataclass
+import pandas as pd
+from common import rename_columns
+from additional import fill_missing_with_median,fill_missing_with_frequency,result_df_preprocessing
+
+
+@dataclass
+class DataIngestionConfig:
+    base_data_path: str = '../../../artifacts/'
+
+class transformation:
+    def __init__(self):
+        self.ingestion_config=DataIngestionConfig()
+
+    def row_data_transformation(self):
+        y = 'Planet Name'
+        data = pd.read_csv(os.path.join(self.ingestion_config.base_data_path, "data.csv"))
+        planets = pd.read_csv(os.path.join(self.ingestion_config.base_data_path, "planets.csv"))
+        # src/Planets/Conponents
+        rename_columns(data,planets["Long_Name"])
+
+        result_df = fill_missing_with_median(data) #Median Imputation
+        result_df = fill_missing_with_frequency(result_df) # Categorical Frequency Imputation
+        result_df = result_df_preprocessing(result_df) # Other necessary preprocessing
+        result_df.isnull().sum()
+        result_df.to_csv(os.path.join(self.ingestion_config.base_data_path, "designed_data.csv", index=False))
+
+
