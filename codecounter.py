9f947a460b3a87ca56392d90bf0ba5ab0f3d34a6
Doğuhan İlter <doguhannilt@gmail.com>
2023-10-06 19:42:31 +0300

    Pyfiglet & Access

--- codecounter.py
+++ codecounter.py
@@ -1,7 +1,20 @@
 import os
+import pyfiglet
+from colorama import init, Fore, Style
 
+# Colorama'nın başlatılması
+init()
 
-# This function counts lines in the files that have .ipynb .py extensions
+# Figlet ile metni büyük ve şekilli bir şekilde yazdırma fonksiyonu
+def print_large_text(text, color=Fore.WHITE, bold=True):
+    f = pyfiglet.Figlet()
+    ascii_art = f.renderText(text)
+    if bold:
+        ascii_art = "\033[1m" + ascii_art
+    colored_ascii_art = color + ascii_art + Style.RESET_ALL
+    print(colored_ascii_art)
+
+# Bu fonksiyon, belirtilen uzantılara sahip dosyalardaki toplam satır sayısını döndürür
 def count_lines_in_files_with_extensions(extensions):
     total_lines = 0
 
@@ -18,4 +31,7 @@ def count_lines_in_files_with_extensions(extensions):
 if __name__ == "__main__":
     extensions_to_count = (".ipynb", ".py")
     total_lines = count_lines_in_files_with_extensions(extensions_to_count)
-    print(f"Total lines in files: {total_lines}")
+
+    # Renkli ve büyük yazıyla metni profesyonel bir şekilde yazdırın
+    text_to_display = f"Bugün Yazılan Toplam Kod Sayısı: {total_lines}"
+    print_large_text(text_to_display, color=Fore.WHITE)
